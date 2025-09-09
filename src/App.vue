cf605444ae374481bc2eedac87fed56a0e74a209
shoaibalirts <shoaibali.rts@gmail.com>
2025-09-09 17:33:56 +0200

    added vue prject necessary files

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-sky-50">
+    <HelloWorld msg="You did it" />
+    <HelloWorld msg="Nice job" />
+  </div>
+</template>
