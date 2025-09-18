37919812488763d5d89d37b7be6dea1f0954e3ed
shoaibalirts <shoaibali.rts@gmail.com>
2025-09-18 17:21:51 +0200

    conditional rendering based on content

--- src/components/TheHeadline.vue
+++ src/components/TheHeadline.vue
@@ -1,6 +1,10 @@
 <template>
-  <p>{{ action }} for everyone</p>
-  <p>Find your next job at PBA Corp.</p>
+  <h1>
+    <span :class="actionClasses">{{ action }}</span
+    ><br />
+    for everyone
+  </h1>
+  <h2>Find your next job at PBA Corp.</h2>
 </template>
 
 <script>
@@ -11,6 +15,16 @@ export default {
       action: 'Build',
       interval: null,
     }
+  },
+  computed: {
+    actionClasses() {
+      return {
+        build: this.action === 'Build',
+        create: this.action === 'Create',
+        design: this.action === 'Design',
+        code: this.action === 'Code',
+      }
+    },
   },
   created() {
     this.changeTitle()
@@ -31,3 +45,17 @@ export default {
   },
 }
 </script>
+<style scoped>
+.build {
+  color: #1a73e8;
+}
+.create {
+  color: #34a853;
+}
+.design {
+  color: #f9ab00;
+}
+.code {
+  color: #d93025;
+}
+</style>
